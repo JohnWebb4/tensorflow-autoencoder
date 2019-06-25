90d978493c8af9cb5642d50c3fe6ce0d34028061
John <John.Webb94@gmail.com>
2019-06-24 23:33:26 -0700

    Initial setup of deepfake

---
+++ src/deepfake.py
@@ -0,0 +1,208 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import tensorflow as tf
+
+import glob
+import sys
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+import PIL
+
+from IPython import display
+
+if len(sys.argv) < 3:
+    raise ValueError('Missing train folder path or test folder path')
+
+train_folder_path = sys.argv[1]
+test_folder_path = sys.argv[2]
+
+train_file_paths = glob.glob('{}/*'.format(train_folder_path))
+test_file_paths = glob.glob('{}/*'.format(test_folder_path))
+
+INPUT_MAX_DIM = 512
+
+
+def load_img(path_to_img):
+    img = tf.io.read_file(path_to_img)
+    img = tf.image.decode_jpeg(img, channels=3)
+    img = tf.image.convert_image_dtype(img, tf.float32)
+
+    shape = tf.cast(tf.shape(img)[:-1], tf.float32)
+    long_dim = tf.math.reduce_max(shape)
+    scale = INPUT_MAX_DIM / long_dim
+
+    new_shape = tf.cast(shape * scale, tf.int32)
+
+    img = tf.image.resize(img, new_shape)
+    img = img[tf.newaxis, :]
+
+    return img
+
+
+train_images = tf.stack([load_img(train_path) for train_path in train_file_paths])
+test_images = tf.stack([load_img(test_path) for test_path in test_file_paths])
+
+train_images = tf.reshape(train_images, [train_images.shape[0], INPUT_MAX_DIM, INPUT_MAX_DIM, 1]).astype("float32")
+test_images = tf.reshape(test_images, [test_images.shape[0], INPUT_MAX_DIM, INPUT_MAX_DIM, 1]).astype("float32")
+
+# Normalize
+train_images /= 255
+test_images /= 255
+
+TRAIN_BUF = 60000
+BATCH_SIZE = 100
+
+TEST_BUF = 10000
+
+train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(TRAIN_BUF).batch(BATCH_SIZE)
+test_dataset = tf.data.Dataset.from_tensor_slices(test_images).shuffle(TEST_BUF).batch(BATCH_SIZE)
+
+
+class CVAE(tf.keras.Model):
+    def __init__(self, latent_dim):
+        super(CVAE, self).__init__()
+        self.latent_dim = latent_dim
+        self.inference_net = tf.keras.Sequential(
+            [
+                tf.keras.layers.InputLayer(input_shape=(28, 28, 1)),
+                tf.keras.layers.Conv2D(
+                    filters=32, kernel_size=3, strides=(2, 2), activation='relu'),
+                tf.keras.layers.Conv2D(
+                    filters=64, kernel_size=3, strides=(2, 2), activation='relu'),
+                tf.keras.layers.Flatten(),
+                # No activation
+                tf.keras.layers.Dense(latent_dim + latent_dim),
+            ]
+        )
+
+        self.generative_net = tf.keras.Sequential(
+            [
+                tf.keras.layers.InputLayer(input_shape=(latent_dim,)),
+                tf.keras.layers.Dense(units=7 * 7 * 32, activation=tf.nn.relu),
+                tf.keras.layers.Reshape(target_shape=(7, 7, 32)),
+                tf.keras.layers.Conv2DTranspose(
+                    filters=64,
+                    kernel_size=3,
+                    strides=(2, 2),
+                    padding="SAME",
+                    activation='relu'),
+                tf.keras.layers.Conv2DTranspose(
+                    filters=32,
+                    kernel_size=3,
+                    strides=(2, 2),
+                    padding="SAME",
+                    activation='relu'),
+                # No activation
+                tf.keras.layers.Conv2DTranspose(
+                    filters=1, kernel_size=3, strides=(1, 1), padding="SAME"),
+            ]
+        )
+
+    def sample(self, eps=None):
+        if eps is None:
+            eps = tf.random.normal(shape=(100, self.latent_dim))
+        return self.decode(eps, apply_sigmoid=True)
+
+    def encode(self, x):
+        mean, logvar = tf.split(self.inference_net(x), num_or_size_splits=2, axis=1)
+        return mean, logvar
+
+    def reparameterize(self, mean, logvar):
+        eps = tf.random.normal(shape=mean.shape)
+        return eps * tf.exp(logvar * .5) + mean
+
+    def decode(self, z, apply_sigmoid=False):
+        logits = self.generative_net(z)
+        if apply_sigmoid:
+            probs = tf.sigmoid(logits)
+            return probs
+
+        return logits
+
+
+optimizer = tf.keras.optimizers.Adam(1e-4)
+
+
+def log_normal_pdf(sample, mean, logvar, raxis=1):
+    log2pi = tf.math.log(2. * np.pi)
+    return tf.reduce_sum(
+        -.5 * ((sample - mean) ** 2. * tf.exp(-logvar) + logvar + log2pi),
+        axis=raxis)
+
+
+def compute_loss(model, x):
+    mean, logvar = model.encode(x)
+    z = model.reparameterize(mean, logvar)
+    x_logit = model.decode(z)
+
+    cross_ent = tf.nn.sigmoid_cross_entropy_with_logits(logits=x_logit, labels=x)
+    logpx_z = -tf.reduce_sum(cross_ent, axis=[1, 2, 3])
+    logpz = log_normal_pdf(z, 0., 0.)
+    logqz_x = log_normal_pdf(z, mean, logvar)
+    return -tf.reduce_mean(logpx_z + logpz - logqz_x)
+
+
+def compute_gradients(model, x):
+    with tf.GradientTape() as tape:
+        loss = compute_loss(model, x)
+    return tape.gradient(loss, model.trainable_variables), loss
+
+
+def apply_gradients(optimizer, gradients, variables):
+    optimizer.apply_gradients(zip(gradients, variables))
+
+
+epochs = 100
+latent_dim = 50
+num_examples_to_generate = 16
+
+# keeping the random vector constant for generation (prediction) so
+# it will be easier to see the improvement.
+random_vector_for_generation = tf.random.normal(
+    shape=[num_examples_to_generate, latent_dim])
+model = CVAE(latent_dim)
+
+
+def generate_and_save_images(model, epoch, test_input):
+    predictions = model.sample(test_input)
+    fig = plt.figure(figsize=(4, 4))
+
+    for i in range(predictions.shape[0]):
+        plt.subplot(4, 4, i + 1)
+        plt.imshow(predictions[i, :, :, 0], cmap='gray')
+        plt.axis('off')
+
+    # tight_layout minimizes the overlap between 2 sub-plots
+    plt.savefig('image_at_epoch_{:04d}.png'.format(epoch))
+
+
+generate_and_save_images(model, 0, random_vector_for_generation)
+
+for epoch in range(1, epochs + 1):
+    start_time = time.time()
+    for train_x in train_dataset:
+        gradients, loss = compute_gradients(model, train_x)
+        apply_gradients(optimizer, gradients, model.trainable_variables)
+    end_time = time.time()
+
+    if epoch % 1 == 0:
+        loss = tf.keras.metrics.Mean()
+        for test_x in test_dataset:
+            loss(compute_loss(model, test_x))
+        elbo = -loss.result()
+        display.clear_output(wait=False)
+        print('Epoch: {}, Test set ELBO: {}, '
+              'time elapse for current epoch {}'.format(epoch,
+                                                        elbo,
+                                                        end_time - start_time))
+        generate_and_save_images(
+            model, epoch, random_vector_for_generation)
+
+
+def display_image(epoch_no):
+    return PIL.Image.open('image_at_epoch_{:04d}.png'.format(epoch_no))
+
+
+plt.imshow(display_image(epochs))
+plt.axis('off')  # Display images
